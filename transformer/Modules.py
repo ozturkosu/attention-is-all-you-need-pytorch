1e06ef5f6830e327649ff2a49497a828caa9595e
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-14 16:09:37 -0700

    comment

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -2,11 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-__author__ = "Yu-Hsiang Huang"
+
 
 class ScaledDotProductAttention(nn.Module):
     ''' Scaled Dot-Product Attention '''
 
+    #Temperature is d 
+    
     def __init__(self, temperature, attn_dropout=0.1):
         super().__init__()
         self.temperature = temperature
