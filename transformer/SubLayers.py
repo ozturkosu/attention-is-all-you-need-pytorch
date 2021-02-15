012ae98776e100f94287717afce824f3f7c2bac8
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-14 23:55:55 -0700

    sublayer

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -2,7 +2,8 @@
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from transformer.Modules import ScaledDotProductAttention
+
+
 
 
 
@@ -80,3 +81,28 @@ class PositionwiseFeedForward(nn.Module):
         x = self.layer_norm(x)
 
         return x
+
+
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    #Temperature is d 
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, mask=None):
+
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
+
+        # attn here is attention matrix
+        
+        attn = self.dropout(F.softmax(attn, dim=-1))
+        output = torch.matmul(attn, v)
+
+        return output, attn
