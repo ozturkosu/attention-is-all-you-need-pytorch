a7adccda93c8eb5ce12c6fbdb2a28d68bc75f25f
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-18 02:54:03 -0700

    init

--- transformer/__init__.py
+++ transformer/__init__.py
@@ -1,13 +1,12 @@
 import transformer.Constants
-import transformer.Modules
+import transformer.Models
 import transformer.Layers
 import transformer.SubLayers
-import transformer.Models
 import transformer.Translator
 import transformer.Optim
 
 
 __all__ = [
-    transformer.Constants, transformer.Modules, transformer.Layers,
-    transformer.SubLayers, transformer.Models, transformer.Optim, 
+    transformer.Constants, transformer.Models, transformer.Layers,
+    transformer.SubLayers, transformer.Optim, 
     transformer.Translator]
