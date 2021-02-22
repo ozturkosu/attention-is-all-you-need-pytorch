87d7a32492d2ac07edea5f8a846f1358b429e0c9
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-22 03:47:19 -0700

    try q in v linear

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -62,7 +62,8 @@ class MultiHeadAttention(nn.Module):
 
         #q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
         #k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
-        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        v = self.w_vs(q).view(sz_b, len_v, n_head, d_v)
 
         #v = self.w_vs(q).view(sz_b, len_q, n_head, d_v)
 
