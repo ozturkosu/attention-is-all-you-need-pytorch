11dd98a27d9ee2796636365c97d808426711ee5e
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-22 02:25:52 -0700

    import math

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import math
 
 class MultiHeadAttention(nn.Module):
     ''' Multi-Head Attention module '''
